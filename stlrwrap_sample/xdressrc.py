682e30bc08be0f9c4e85bc8e5df3d0711cb0b8b9
Cory Perkins <jcperkins12@gmail.com>
2018-11-14 19:53:03 -0600

    Successfully implemented the stlwrap example from the xDress tutorial

---
+++ stlrwrap_sample/xdressrc.py
@@ -0,0 +1,12 @@
+package = 'mypack'     # top-level python package name
+packagedir = 'mypack'  # location of the python package
+
+stlcontainers = [
+    ('vector', 'str'),
+    ('set', 'uint'),
+    ('map', 'int', 'float'),
+    ]
+
+# will be used later, but need to be present now
+classes = []
+functions = []
